6bf03fdcb289b51d5f8663a0213aab8c8fbfc510
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-11-14 16:14:25 -0500

    Initialized Project

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
 /**
  * Created by Owner on 9/7/2019.
  */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.sql.*;
+
+public class Main  extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try{
+//            Class.forName("Main Front");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:system/root@localhost:1521:xe");
+            System.out.println("Successfully Connected to the database.... " + con.toString());
+        }catch (Exception e){
+            System.out.println("Could not connect to the database....." + e);
+        }
+
+        
     }
 }
 
